549ee3ac023cb3e45ccb27104cc625d185472d4b
Naveen882003 <snknaveen2003@gmail.com>
2024-10-24 23:22:56 +0530

    patterns commit

---
+++ Basic Problems/Patterns/pattern01.py
@@ -0,0 +1,22 @@
+'''
+*****
+*****
+*****
+*****
+*****
+'''
+
+# method 01
+n= int(input())
+for i in range(n):
+    print('*'*n)
+
+# method 02
+
+n= int(input())
+for i in range(n):
+    for j in range(n):
+        print("*")
+
+
+
